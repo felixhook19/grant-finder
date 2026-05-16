cff0c00da8843ea569b795d47356e5fd69e16367
felixhook19 <felix.hook19@gmail.com>
2026-05-16 16:12:54 +0100

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,44 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
+function getSupabaseCookieHandler() {
+  return {
+    async getAll() {
+      const cookieStore = await cookies()
+      return cookieStore.getAll()
+    },
+    async setAll(cookiesToSet: any[]) {
+      const cookieStore = await cookies()
+      cookiesToSet.forEach((cookie: any) => {
+        cookieStore.set(cookie.name, cookie.value, cookie.options ?? {})
+      })
+    },
+  }
+}
+
 export async function createClient() {
-  const cookieStore = await cookies()
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() { return cookieStore.getAll() },
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        setAll(cookiesToSet: any) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }: any) =>
-              cookieStore.set(name, value, options)
-            )
-          } catch {}
-        },
-      },
-    }
+    { cookies: getSupabaseCookieHandler() }
   )
 }
 
 export async function createAdminClient() {
-  const cookieStore = await cookies()
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!,
-    {
-      cookies: {
-        getAll() { return cookieStore.getAll() },
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        setAll(cookiesToSet: any) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }: any) =>
-              cookieStore.set(name, value, options)
-            )
-          } catch {}
-        },
-      },
-    }
+    { cookies: getSupabaseCookieHandler() }
   )
 }
